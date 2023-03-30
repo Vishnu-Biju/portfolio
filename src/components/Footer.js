5e5c6ae6a4ddcaea4db226e3bce66628b6543a6a
Vishnu Biju <117735067+Vishnu-Biju@users.noreply.github.com>
2023-03-30 18:25:30 +0530

    footer edited

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ export const Footer = () => {
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
 
-            // instead of using # use the link to your social media account to connect with you
+             {/* instead of using # use the link to your social media account to connect with you */}
             
               <a href="#"><img src={navIcon1} alt="Icon" /></a>
               <a href="#"><img src={navIcon2} alt="Icon" /></a>
